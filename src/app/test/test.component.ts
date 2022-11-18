a021e08c83ea8453f6fe7c15529cb48db2f31864
Chazz <czcarrizales@yahoo.com>
2022-11-18 15:54:57 -0800

    test using a template instead of a templateUrl in the test component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'test',
-  templateUrl: './test.component.html',
+  template: `Testing writing html in a template instead of a url.`,
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
