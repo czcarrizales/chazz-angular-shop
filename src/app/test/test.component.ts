af8b9ee82abbd01c40a91b0a2097c83e58d0f83d
Chazz <czcarrizales@yahoo.com>
2022-11-18 15:57:18 -0800

    test using styles directly in the component instead of using a styleUrl

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'test',
   template: `Testing writing html in a template instead of a url.`,
-  styleUrls: ['./test.component.scss']
+  styles: [`
+  p {
+    color: red;
+  }
+  `]
+  // styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
   
