31eafd7904bcfc548cef4a0195168e819d4c8874
Chazz <czcarrizales@yahoo.com>
2022-11-18 15:51:33 -0800

    test using the cli to create a component

---
+++ src/app/test-cli/test-cli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-cli',
+  templateUrl: './test-cli.component.html',
+  styleUrls: ['./test-cli.component.scss']
+})
+export class TestCliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
