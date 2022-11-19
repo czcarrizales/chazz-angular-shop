5ef1f1563bcff9ae9abc1778db90de5fc60f52e5
Chazz <czcarrizales@yahoo.com>
2022-11-18 19:52:14 -0800

    passing and using data with event binding using an input

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,8 +9,13 @@ export class TestComponent {
   name = 'Chazz'
   match = false
   title = 'Property Binding Test'
+  eventTest = ''
 
   buttonClick() {
     this.match = !this.match
+  }
+
+  onTextChange(event: Event) {
+    this.eventTest = (<HTMLInputElement>event.target).value
   }
 }
