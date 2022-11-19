87f954faf46918f607fa15e86ed26e690302cbac
Chazz <czcarrizales@yahoo.com>
2022-11-19 08:56:07 -0800

    test using different forms of of databinding, including property binding, event binding, two way binding, and string interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,9 +10,15 @@ export class TestComponent {
   match = false
   title = 'Property Binding Test'
   eventTest = ''
+  changingText = ''
+  username = ''
 
   buttonClick() {
     this.match = !this.match
+  }
+
+  resetName() {
+    this.username = ''
   }
 
   onTextChange(event: Event) {
