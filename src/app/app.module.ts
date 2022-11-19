11aa1f11515e9709e93813da4c3814051b020270
Chazz <czcarrizales@yahoo.com>
2022-11-19 14:33:17 -0800

    importing components for fantasy shopping app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,26 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { TestCliComponent } from './test-cli/test-cli.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { SpellsComponent } from './spells/spells.component';
+import { SpellListComponent } from './spells/spell-list/spell-list.component';
+import { SpellDetailComponent } from './spells/spell-detail/spell-detail.component';
+import { SpellItemComponent } from './spells/spell-list/spell-item/spell-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    TestCliComponent
+    TestCliComponent,
+    HeaderComponent,
+    SpellsComponent,
+    SpellListComponent,
+    SpellDetailComponent,
+    SpellItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
