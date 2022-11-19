5276123c3ac383d49a8b3f4888e347ee9e55d6eb
Chazz <czcarrizales@yahoo.com>
2022-11-18 18:51:58 -0800

    add name property to test component to test string interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'test',
-  template: `<p>Testing writing html in a template instead of a url.</p>`,
-  styles: [`
-  p {
-    color: red;
-  }
-  `]
-  // styleUrls: ['./test.component.scss']
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-  
+  name = 'Chazz'
 }
