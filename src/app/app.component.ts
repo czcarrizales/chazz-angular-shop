f33930c302bfecf8d32709e37530d6637787b1a8
Chazz <czcarrizales@yahoo.com>
2022-11-19 21:26:42 -0800

    experimenting with @Input() decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chazz-angular-shop';
+  testSpells = ['hello', 'goodbye']
 }
--- src/app/spells/spells.component.ts
+++ src/app/spells/spells.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spells',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spells.component.scss']
 })
 export class SpellsComponent implements OnInit {
+
+  @Input() inputTest!: string;
 
   constructor() { }
 
