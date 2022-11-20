902120ee5c8d03d7dee17735a38c92af7ef68fc8
Chazz <czcarrizales@yahoo.com>
2022-11-19 19:38:18 -0800

    import Spell model and create new Spell object in spells array (spell-list.component.ts)

--- src/app/spells/spell-list/spell-list.component.ts
+++ src/app/spells/spell-list/spell-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Spell } from '../spell.model';
 
 @Component({
   selector: 'app-spell-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spell-list.component.scss']
 })
 export class SpellListComponent implements OnInit {
+
+  spells: Spell[] = [
+    new Spell('Fireball', 'Fire scorches their faces', '#')
+  ]
 
   constructor() { }
 
