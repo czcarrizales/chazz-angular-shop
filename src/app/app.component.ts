21a2ea5679f6d1c65221290ea6049fe60c1559a7
Chazz <czcarrizales@yahoo.com>
2022-11-20 17:26:37 -0800

    add oddNumbers and evenNumbers array and onNavigate function that will take an event and replace loadedFeature value with that incoming string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,24 @@ export class AppComponent {
   parentEgg = 'Spiked Dragon Egg'
   fireworks = ['rainbow', 'dark and cloudy']
 
+  oddNumbers: number[]= []
+  evenNumbers: number[]= []
+
+  loadedFeature = 'spell'
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  addOddOrEven(oddOrEven: number) {
+    if (oddOrEven % 2 === 0) {
+      this.evenNumbers.push(oddOrEven)
+    } else if (oddOrEven % 2 === 1) {
+      this.oddNumbers.push(oddOrEven)
+    }
+    console.log(oddOrEven)
+  }
+
   addFirework(newFireworkEvent: string) {
     this.fireworks.push(newFireworkEvent)
   }
