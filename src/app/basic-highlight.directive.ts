2024cc3fb419669c6c7ca2528112f943066c608f
czcarrizales <czcarrizales@yahoo.com>
2022-11-21 12:07:19 -0800

    learning about HostBinding instead of using Renderer2

--- src/app/basic-highlight.directive.ts
+++ src/app/basic-highlight.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 
 export class BasicHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   @HostListener('mouseover') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey')
+    this.backgroundColor = 'purple'
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+    this.backgroundColor = 'red'
   }
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
