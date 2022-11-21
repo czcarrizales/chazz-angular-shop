3ed34ff3a52e2d11cc16cd319103eadcdba1ce98
czcarrizales <czcarrizales@yahoo.com>
2022-11-21 12:03:28 -0800

    learn about using @HostListener to react to mouse event, such as changing the background color when hovering a mouse over an element and then changing the background color again when the mouse leaves the element

--- src/app/basic-highlight.directive.ts
+++ src/app/basic-highlight.directive.ts
@@ -1,15 +1,23 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 
 export class BasicHighlightDirective implements OnInit {
+  @HostListener('mouseover') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey')
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+  }
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
   }
 
   ngOnInit(): void {
-      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey')
+      // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey')
   }
 }
