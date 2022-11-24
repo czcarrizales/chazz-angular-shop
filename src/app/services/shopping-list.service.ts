a76f71416342eeef598f0f2a2e2a713312fb4fb6
Chazz <czcarrizales@yahoo.com>
2022-11-23 18:15:30 -0800

    add ingredients array to shopping-list service, and create methods for getting and adding ingredients

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,3 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
+  ingredients: Ingredient[] = [
+    new Ingredient('Black Rose Petal', 5),
+    new Ingredient('Explosive Powder Pellet', 2),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice()
+  }
+  addIngredient() {
     
+  }
 }
