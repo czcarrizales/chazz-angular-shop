e20e14591ccc0a801b81098a1ea2c7c6244bc421
Chazz <czcarrizales@yahoo.com>
2022-11-26 12:09:33 -0800

    add a new Ingredient to ingredients array for dummy spells

--- src/app/services/spell.service.ts
+++ src/app/services/spell.service.ts
@@ -7,7 +7,7 @@ export class SpellService {
 
     private spells: Spell[] = [
         new Spell('Fireball', 'Fire scorches their faces', 'https://www.pngkit.com/png/detail/942-9426503_fireball-boladefogo-fire-fogo-bola-ball-effect-fireball.png', [new Ingredient('Fire Flower', 1)]),
-        new Spell('Lightning Bolt', 'Scar the blood between their veins with volts of electricity', 'https://helios-i.mashable.com/imagery/articles/03bDuXus9Lsnz9sUqURbDiB/hero-image.fill.size_1200x1200.v1643714839.jpg', [new Ingredient('Lightning Lily', 2)])
+        new Spell('Lightning Bolt', 'Scar the blood between their veins with volts of electricity', 'https://helios-i.mashable.com/imagery/articles/03bDuXus9Lsnz9sUqURbDiB/hero-image.fill.size_1200x1200.v1643714839.jpg', [new Ingredient('Lightning Lily', 2), new Ingredient ('Old Volt Rod', 6)])
       ]
 
       getSpells() {
