9dc90c7fdba02fad9eec91580893cf2b2c9a6ad6
Chazz <czcarrizales@yahoo.com>
2022-11-26 12:33:33 -0800

    add routes to spells and shopping-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { SpellsComponent } from './spells/spells.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/spells', pathMatch: 'full'},
+  {path: 'spells', component: SpellsComponent},
+  {path: 'shopping-list', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
