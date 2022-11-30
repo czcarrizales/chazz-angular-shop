c78bc5ae16bc0a3cd63e47d238b797585cf3f2e1
Chazz <czcarrizales@yahoo.com>
2022-11-29 19:07:45 -0800

    formatting and adding a Subject that will take an index number for editing an item

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,10 +1,10 @@
 import { EventEmitter } from '@angular/core';
-import {Subject} from 'rxjs'
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-    ingredientsChanged = new Subject<Ingredient[]>();
-
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>()
   private ingredients: Ingredient[] = [
     new Ingredient('Black Rose Petal', 5),
     new Ingredient('Explosive Powder Pellet', 2),
@@ -14,11 +14,11 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
   addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient)
-    this.ingredientsChanged.next(this.ingredients.slice())
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
   sendIngredientsToShoppingList(ingredient: Ingredient) {
-    this.ingredients.push(ingredient)
-    this.ingredientsChanged.next(this.ingredients.slice())
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
